46c880f63d8dd345b526235a23d61492e819d709
Wilsonalves96 <wilsonalves96@live.com>
2015-05-16 14:45:45 -0300

    No changes
    Commit because Checkout files

--- DraftStore2/src/java/com/wrm/draftstore/servlets/autenticacao/LoginServlet.java
+++ DraftStore2/src/java/com/wrm/draftstore/servlets/autenticacao/LoginServlet.java
@@ -23,9 +23,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
